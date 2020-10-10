82f447e3ffd61bbe76b44f7db04c5370cdfd0f00
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2020-10-10 15:43:49 -0300

    Aula Webdesign

---
+++ 03 - WEBDESIGN/01_Apostilas/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='robots' content='index, follow'>
+    <link rel='shortcut icon' href='assets/img/ifto.svg'>
+    <link rel='stylesheet' href='assets/css/style.css'>
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            Seja bem vindo ao meu site
+        </h1>
+        <p>Exemplo de seletores</p>
+    </header>
+
+    <main>
+        <section class="main_blog">
+            <header>
+                <h2>Sessão Principal de Conteúdo</h2>
+            </header>
+
+            <article>
+                <header>
+                    <h3 style="color:blue">Teste de Artigo</h3>
+                </header>
+                <p>
+                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
+                    odit
+                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
+                    odit
+                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
+                    odit
+                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
+                    odit
+                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
+                </p>
+            </article>
+        </section>
+
+    </main>
+    <article>
+        <header>
+            <h3>Teste de Artigo</h3>
+        </header>
+        <p>
+            In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut odit
+            fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
+        </p>
+    </article>
+    <p>
+        Isso aqui é um bloco
+    </p>
+
+    <script> </script>
+</body>
+
+</html>
--- www/index.php
+++ www/index.php
@@ -1,85 +1,103 @@
-<!DOCTYPE html>
+<!DOCTYPE html> <!-- DOCUMENTO HTML 5 -->
 <html lang='pt-br'>
+<!-- INFORMAR A LÍNGUA QUE SERÁ UTILIZADA NO SITE -->
 
 <head>
+    <!-- CONJUNTO DE CARACTERES -->
     <meta charset='UTF-8'>
+
+    <!-- VIEWPORT -->
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+    <!-- ROBOTS -->
     <meta name='robots' content='index, follow'>
-    <link rel='shortcut icon' href='assets/img/ifto.svg'>
-    <link rel='stylesheet' href='assets/css/style.css'>
-    <title>Document</title>
+
+    <!-- SHORTCUT -->
+    <link rel='shortcut icon' href='http://www.ifto.edu.br/++theme++portal-do-ifto/img/favicon.ico'>
+
+    <!-- CSS -->
+    <link rel='stylesheet' href=''>
+
+    <!-- TÍTULO DA PÁGINA -->
+    <title>APRENDENDO HTML</title>
 </head>
 
+<!-- CONTEÚDO EXIBIDO PARA O USUÁRIO  -->
+
 <body>
-    <header>
-        <h1>
-            Seja bem vindo ao meu site
-        </h1>
-        <p>Exemplo de seletores</p>
-    </header>
+    <h1> Em relação aos jogos da atual geração, a Sony afirma que apenas dez títulos ficam confinados ao PS4. Eles serão
+        designados como “jogáveis apenas no PS4” na PlayStation Store e não poderão ser instalados e executados no PS5
+    </h1>
+    <!-- <h2> h2 </h2>
+    <h3> h3 </h3>
+    <h4> h4 </h4>
+    <h5> h5 </h5> -->
 
-    <main>
-        <section class="main_blog">
-            <header>
-                <h2>Sessão Principal de Conteúdo</h2>
-            </header>
-
-            <article>
-                <header>
-                    <h3 style="color:blue">Teste de Artigo</h3>
-                </header>
-                <p>
-                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
-                    odit
-                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
-                </p>
-            </article>
-            <article>
-                <header>
-                    <h3>Teste de Artigo</h3>
-                </header>
-                <p>
-                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
-                    odit
-                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
-                </p>
-            </article>
-            <article>
-                <header>
-                    <h3>Teste de Artigo</h3>
-                </header>
-                <p>
-                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
-                    odit
-                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
-                </p>
-            </article>
-            <article>
-                <header>
-                    <h3>Teste de Artigo</h3>
-                </header>
-                <p>
-                    In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut
-                    odit
-                    fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
-                </p>
-            </article>
-        </section>
-
-    </main>
-    <article>
-        <header>
-            <h3>Teste de Artigo</h3>
-        </header>
-        <p>
-            In sed consequatur modi sapiente dolorem dolores. Ut laboriosam qui laboriosam. Quas sit et non ut odit
-            fugiat sit. Odit recusandae omnis ipsum necessitatibus voluptas.
-        </p>
-    </article>
     <p>
-        Isso aqui é um bloco
+        <!-- MARCAÇÃO DE PARÁGRAFOS  -->
+        A respeito dos demais, a Sony observa que os jogos podem oferecer o modo Game Boost. O recurso faz com que o
+        game
+        reconheça o hardware mais poderoso do PS5 para rodar melhor do que no PS4, com gráficos superiores e performance
+        mais estável, por exemplo. Para tirar proveito dessas vantagens, a recomendação é de manter sempre o PS5 e os
+        títulos atualizados nas últimas versões.
     </p>
 
+    <P>
+        Também há ressalvas de que mesmo jogos compatíveis com o PS5 podem apresentar algum tipo de limitação em termos
+        de
+        recursos originalmente suportados no PS4, além de erros e eventuais problemas de performance. Porém, a Sony não
+        dá
+        nenhum exemplo do que seriam esses recursos e recomenda que o usuário teste cada game, antes de comprar DLCs e
+        conteúdo adicional para títulos de PS4.
+    </P>
+
+    <P>
+        Em relação aos jogos da atual geração, a Sony afirma que apenas dez títulos ficam confinados ao PS4. Eles serão
+        designados como “jogáveis apenas no PS4” na PlayStation Store e não poderão ser instalados e executados no PS5
+    </P>
+
+    <a href="http://www.ifto.edu.br/araguaina" target="blank" title="Campus Araguaina" alt="Campus Araguaina">
+        Campus Araguaína
+    </a>
+
+    <h2> Lista de jogos incompatíveis com o PlayStation 5 </h2>
+
+    <ul>
+        <li>DWVR</li>
+        <li>Afro Samurai 2 Revenge of Kuma Volume One</li>
+        <li>TT Isle of Man - Ride on the Edge 2</li>
+        <li>Just Deal With It!</li>
+        <li>Shadow Complex Remastered</li>
+        <li>Robinson: The Journey</li>
+        <li>We Sing</li>
+        <li>Hitman Go: Definitive Edition</li>
+        <li>Shadwen</li>
+        <li>Joe's Diner</li>
+    </ul>
+
+    <!-- <ol reversed> -->
+    <!-- <ol start=3> -->
+    <!-- <ol type='I'> -->
+    <!-- <ol type='A'> -->
+    <ol type='a'>
+        <li>DWVR</li>
+        <li>Afro Samurai 2 Revenge of Kuma Volume One</li>
+
+        <ul>
+            <li>Programador WEB</li>
+        </ul>
+
+        <li>TT Isle of Man - Ride on the Edge 2</li>
+        <li>Just Deal With It!</li>
+        <li>Shadow Complex Remastered</li>
+        <li>Robinson: The Journey</li>
+        <li>We Sing</li>
+        <li>Hitman Go: Definitive Edition</li>
+        <li>Shadwen</li>
+        <li>Joe's Diner</li>
+    </ol>
+
+    <!-- SCRIPT (É RECOMENDADO COLOCAR NO FINAL DO BODY)  -->
     <script> </script>
 </body>
 
