62e9a48a63c9c579c48ed88354bd1915b1cc22a4
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2020-10-17 21:01:14 -0300

    atualizando

---
+++ www/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='robots' content='index, follow'>
+    <link rel='shortcut icon' href='assets/img/ifto.svg'>
+    <link rel='stylesheet' href='assets/css/boot.css'>
+    <link rel='stylesheet' href='assets/css/style.css'>
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            Seja bem vindo ao meu site
+        </h1>
+        <p>Exemplo de seletores</p>
+    </header>
+
+    <main>
+
+        <a href="#" class="btn btn_green">Botão</a>
+        <a href="#" class="btn btn_red">Excluir</a>
+
+        <section class="main_blog radius">
+            <header>
+                <h2>Sessão Principal de Conteúdo</h2>
+            </header>
+
+            <article class="radius">
+                <header>
+                    <h3 class="title_medium_lightgray">Teste de Artigo</h3>
+                </header>
+                <p>
+                    Error dolorem molestiae odio cum adipisci iste. Ipsam voluptas nihil beatae aperiam perspiciatis
+                    nihil
+                    molestiae ducimus. Eveniet dicta ratione rerum iste voluptatem dolor dolor. Ea quam ut neque nihil
+                    quis.
+                    Recusandae harum ut id consequatur excepturi illum provident libero.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    Error dolorem molestiae odio cum adipisci iste. Ipsam voluptas nihil beatae aperiam perspiciatis
+                    nihil
+                    molestiae ducimus. Eveniet dicta ratione rerum iste voluptatem dolor dolor. Ea quam ut neque nihil
+                    quis.
+                    Recusandae harum ut id consequatur excepturi illum provident libero.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    Error dolorem molestiae odio cum adipisci iste. Ipsam voluptas nihil beatae aperiam perspiciatis
+                    nihil
+                    molestiae ducimus. Eveniet dicta ratione rerum iste voluptatem dolor dolor. Ea quam ut neque nihil
+                    quis.
+                    Recusandae harum ut id consequatur excepturi illum provident libero.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    Error dolorem molestiae odio cum adipisci iste. Ipsam voluptas nihil beatae aperiam perspiciatis
+                    nihil
+                    molestiae ducimus. Eveniet dicta ratione rerum iste voluptatem dolor dolor. Ea quam ut neque nihil
+                    quis.
+                    Recusandae harum ut id consequatur excepturi illum provident libero.
+                </p>
+            </article>
+
+            <article>
+                <header>
+                    <h3>Teste de Artigo</h3>
+                </header>
+                <p>
+                    Error dolorem molestiae odio cum adipisci iste. Ipsam voluptas nihil beatae aperiam perspiciatis
+                    nihil
+                    molestiae ducimus. Eveniet dicta ratione rerum iste voluptatem dolor dolor. Ea quam ut neque nihil
+                    quis.
+                    Recusandae harum ut id consequatur excepturi illum provident libero.
+                </p>
+            </article>
+        </section>
+
+    </main>
+    <article>
+        <header>
+            <h3>Teste de Artigo</h3>
+        </header>
+        <p>
+            Error dolorem molestiae odio cum adipisci iste. Ipsam voluptas nihil beatae aperiam perspiciatis nihil
+            molestiae ducimus. Eveniet dicta ratione rerum iste voluptatem dolor dolor. Ea quam ut neque nihil quis.
+            Recusandae harum ut id consequatur excepturi illum provident libero.
+        </p>
+    </article>
+    <p>
+        Isso aqui é um bloco
+    </p>
+    <script> </script>
+</body>
+
+</html>
